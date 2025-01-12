e5364108ff9c451c2d1278b8567692537f5586b7
Kostia Stoliarskiy <saintkos117@gmail.com>
2025-01-12 16:18:39 +0200

    Add debugging configuration and update mongoose version; enhance server error handling

--- bin/server.js
+++ bin/server.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose')
 const app = require('../app')
 
 const { DB_HOST, PORT = 3000 } = process.env
+
+mongoose.set('strictQuery', true) // Додайте цей рядок, щоб уникнути попередження
+
 mongoose
-  .connect(DB_HOST)
-  .then(() => app.listen(PORT))
-  .catch((error) => {
-    console.log(error.message)
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  })
+  .catch(err => {
+    console.log(`Server not running. Error message: ${err.message}`)
     process.exit(1)
   })
